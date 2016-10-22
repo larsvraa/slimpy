db81d99bad551b1818ba1e3edfbae6518f940bd1
Lars Vraa <lars.vraa@gmail.com>
2016-10-23 00:38:37 +0200

    changed featured image height

--- functions.php
+++ functions.php
@@ -61,7 +61,7 @@ function slimpy_setup() {
    */
   add_theme_support( 'post-thumbnails' );
 
-  add_image_size( 'slimpy-featured', 750, 410, true );
+  add_image_size( 'slimpy-featured', 750, 340, true );
   add_image_size( 'tab-small', 60, 60 , true); // Small Thumbnail
 
   // This theme uses wp_nav_menu() in one location.
